8a903d92a2c2466165040265d711ff6eac13206d
hatsumi3 <al.usamiku39@gmail.com>
2021-07-25 17:39:04 +0900

    create user api.

--- server.go
+++ server.go
@@ -4,15 +4,48 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
 )
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
 func main() {
 	e := echo.New()
+	e.Logger.SetLevel(log.DEBUG)
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
 
-	var greed string = "Hello, World!"
+	e.GET("/", hello)
+	e.GET("/user", show)
+	e.POST("/user", display)
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, greed)
-	})
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func hello(c echo.Context) error {
+	var greed string = "Hello, World!"
+	return c.String(http.StatusOK, greed)
+}
+
+func show(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	c.Logger().Info(u)
+	return c.JSON(http.StatusOK, u)
+}
+
+func display(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	c.Logger().Info(u)
+	return c.JSON(http.StatusCreated, u)
 }
